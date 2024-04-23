83e9d2ed3b6fc262b7566ea8fb4de1f622172e4d
Mike Haller <mike.haller@bosch.com>
2024-04-23 14:30:08 +0000

    Use PATH for ghz

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ for f in $FILES
 do
   echo "Processing $f file..."
   # take action on each file. $f store current file name
-  ./ghz --config=$f --import-paths=kuksa-databroker/proto/ $@
+  ghz --config=$f --import-paths=kuksa-databroker/proto/ $@
 done
